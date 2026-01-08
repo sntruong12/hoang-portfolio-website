5f88d5655a43c29574ecef15bfa07a271609aa47
son truong <sntruong12@gmail.com>
2026-01-08 00:14:31 -0600

    initial work for project carousel

---
+++ js/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+console.log("it's alive")
+
+const projects = [
+    {
+        id: 1,
+        title: "DedCool",
+        description: "DedCool is an LA based fragrance brand with a mission to empower individuals through scent. They approached me to concept and create a video to live on social that targets Gen Z audiences inspired by Japanese Retro aesthetics.",
+        client: "DedCool",
+        type: "Social Animation",
+        role: "Illustrator & Animator",
+        media: {
+            featured: "./media/project-1/featured.svg",
+            count: 5,
+        },
+        colors: {
+            bg: "#F7DEC1",
+            navItems: "#000000",
+            navContact: "#F4835D",
+            navLogo: "black",
+        }
+    },
+    {
+        id: 2,
+        title: "Somerset",
+        description: "Somerset is a Gold Coast restaurant located within the Viceroy Hotel that combines 1960s Country Club aesthetics with imagery inspired by Lake Michigan Marina. This branding concept brings a contemporary approach to a historic crest and uses a slightly feminine palette to give the visuals a soft and approachable atmosphere.",
+        client: "Boka Restaurant Group",
+        type: "Somerset Branding",
+        role: "Lead Designer",
+        media: {
+            featured: "./media/project-2/featured.svg",
+            count: 4,
+        },
+        colors: {
+            bg: "#FFCBCB",
+            navItems: "#000000",
+            navContact: "#4C6E56",
+            navLogo: "black",
+        }
+    },
+]
